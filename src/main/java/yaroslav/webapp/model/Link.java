602719cb38b1b90ff0f1c53d9d6f196e8a5c19e3
Ярослав <yaroslav219952@gmail.com>
2015-09-08 15:32:08 +0300

    First commit

---
+++ src/main/java/yaroslav/webapp/model/Link.java
@@ -0,0 +1,10 @@
+package yaroslav.webapp.model;
+
+/**
+ * Created by ярослав on 08.09.2015.
+ */
+public class Link {
+    private String name;
+    private String url;
+
+}
---
+++ src/main/java/yaroslav/webapp/model/Resume.java
@@ -0,0 +1,11 @@
+package yaroslav.webapp.model;
+
+import java.util.Collection;
+
+/**
+ * Created by ярослав on 08.09.2015.
+ */
+public class Resume {
+   private String fullName;
+    private Collection<String> collection;
+}
